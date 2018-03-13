3437679a179d3de05dcba1755cfb205b7de2bff4
Luke Huxley <luke.huxley@cloudpay.net>
2018-03-13 20:06:26 +0000

    Nuke!

--- resources/views/includes/join-scrum.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="panel {{ $panel_class }}">
-    <div class="panel-heading">{!! $title !!}</div>
-    <div class="list-group">
-        @foreach ($scrums as $scrum)
-            <a class="list-group-item" href="/{{ $scrum->uuid }}">{{ $scrum->name }}<span class="badge badge-default badge-pill">{{ count($scrum->voters()->get()) }}</span><span class="badge badge-default badge-pill">{{ $scrum->updated_at }}</span></a>
-        @endforeach
-    </div>
-</div>
-
